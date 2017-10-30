2d4b6656084cd720285467aad023ac5bf24a39a3
riguang <1029367878@qq.com>
2017-10-30 22:15:14 +0800

    解决了build出错，webpack配置了preact，antd不完全支持

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,15 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
-const rewirePreact = require('react-app-rewire-preact');
+//const rewirePreact = require('react-app-rewire-preact');
 const rewireMobX = require('react-app-rewire-mobx');
 module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config);
     config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
     // use the Preact rewire
-    if (env === "production") {
-        console.log("⚡ Production build with Preact");
-        config = rewirePreact(config, env);
-    }
+    // if (env === "production") {
+    //     console.log("⚡ Production build with Preact");
+    //     config = rewirePreact(config, env);
+    // }
     config = rewireLess(config, env, {
         modifyVars: { "@primary-color": "#1DA57A" },
     });
